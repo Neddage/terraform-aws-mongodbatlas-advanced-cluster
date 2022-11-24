da80175ae92830910736799b0c33422128a21337
Ian Shaw <ian.shaw@digitaltactics.co.uk>
2022-11-24 16:42:01 +0000

    fmt

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -8,10 +8,10 @@ provider "mongodbatlas" {}
 module "atlas_advanced_cluster" {
   source = "../../"
 
-  project_name="my-atlas-project"
-  org_id="996228199622819962281"
-  region="eu-west-2"
-  
-  cluster_name = "my-cluster"
+  project_name = "my-atlas-project"
+  org_id       = "996228199622819962281"
+  region       = "eu-west-2"
+
+  cluster_name  = "my-cluster"
   instance_size = "M10"
 }
