974f829bb51dd501e5483764950f6e30365b7c9c
Ian Shaw <ian.shaw@digitaltactics.co.uk>
2022-11-24 15:56:38 +0000

    Example updated with new full example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,57 +1,75 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-west-2"
 }
 
 provider "mongodbatlas" {}
 
 
-module "atlas_cluster" {
+module "advanced_atlas_cluster" {
   source = "../../"
 
-  project_name = "my-project"
-  org_id       = "5edf67f9b9614996228111"
-
+  project_name="my-atlas-project"
+  org_id="996228199622819962281"
+  region="eu-west-2"
   teams = {
-    Devops : {
-      users : ["example@mail.io", "user@mail.io"]
-      role : "GROUP_OWNER"
+    Devops: {
+      users: ["example@mail.io", "user@mail.io"]
+      role: "GROUP_OWNER"
     },
-    DevTeam : {
-      users : ["developer@mail.io"]
-      role : "GROUP_READ_ONLY"
+    DevTeam: {
+      users: ["developer@mail.io",]
+      role: "GROUP_READ_ONLY"
     }
   }
-
   white_lists = {
-    "example comment" : "52.12.41.46/32",
-    "second example" : "54.215.4.201/32"
+    "example comment": "10.0.0.1/32",
+    "second example": "10.10.10.8/32"
   }
-
-  region = "EU_WEST_1"
-
-  cluster_name = "MyCluster"
-
-  instance_type      = "M30"
-  mongodb_major_ver  = 4.2
-  cluster_type       = "REPLICASET"
-  num_shards         = 1
-  replication_factor = 3
-  provider_backup    = true
-  pit_enabled        = false
-
-  vpc_peer = {
-    vpc_peer1 : {
-      aws_account_id : "020201234877"
-      region : "eu-west-1"
-      vpc_id : "vpc-0240c8a47312svc3e"
-      route_table_cidr_block : "172.16.0.0/16"
+  cluster_name = "my-cluster"
+  instance_size = "M10"
+  mongodb_major_version = 6.1
+  mongodb_version_release_system = "CONTINUOUS"
+  cluster_type = "REPLICASET"
+  num_shards = 1
+  num_nodes = 3
+  backup_enabled = true
+  pit_enabled =  true
+  disk_size_gb = 20
+  auto_scaling_disk_gb_enabled = true
+  auto_scaling_compute_enabled = true
+  auto_scaling_compute_scale_down_enabled = true
+  auto_scaling_compute_min_instance_size = "M10"
+  auto_scaling_compute_max_instance_size = "M30"
+  volume_type = "STANDARD"
+  disk_iops = 1000
+  encryption_at_rest_enabled = false
+  termination_protection_enabled = true
+  atlas_vpc_cidr_block = "192.168.248.0/21"
+  vpc_peers = {
+    peer1 : {
+      aws_account_id = "890987654321"
+      region = "eu-west-1"
+      vpc_id = "vpc-0420c7a65412fdr3e"
+      route_table_cidr_block = "172.16.0.0/16"
+      add_cidr_to_whitelist = true
+      route_tables = [
+        "rtb-08e4b8ccface7f466",
+        "rtb-18yu75trface7d787",
+        "rtb-77h4b8ccfty75gg46"
+      ] 
     },
-    vpc_peer2 : {
-      aws_account_id : "0205432147877"
-      region : "eu-central-1"
-      vpc_id : "vpc-0f0dd82430bhv0e1a"
-      route_table_cidr_block : "172.17.0.0/16"
+    peer2 : {
+      aws_account_id = "123456789098"
+      region = "eu-west-2"
+      vpc_id = "vpc-0420c7a65412fdr3e"
+      route_table_cidr_block = "172.31.0.0/16"
+      add_cidr_to_whitelist = true
+      route_tables = [
+        "rtb-10e4b8ccface7f771",
+        "rtb-55pu21arface7d707",
+        "rtb-91h4b8frfty75gg46"
+      ] 
     }
-  }
+  }  
 
 }
