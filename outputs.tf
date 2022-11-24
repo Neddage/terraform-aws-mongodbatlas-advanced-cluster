8c531a63acacfbda679032099352d74220d312df
Ian Shaw <ian.shaw@digitaltactics.co.uk>
2022-11-24 13:44:50 +0000

    cleanup output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "everything" {
+  value = mongodbatlas_advanced_cluster.cluster
+}
+
 output "cluster_id" {
   value       = mongodbatlas_advanced_cluster.cluster.cluster_id
   description = "The cluster ID"
@@ -13,41 +17,10 @@ output "mongo_connection_strings" {
   description = "connection strings for the cluster"
 }
 
-output "everything" {
-  value = mongodbatlas_advanced_cluster.cluster
-}
-
 output "container_id" {
-  value       = mongodbatlas_advanced_cluster.cluster.replication_specs.*.container_id[0]
+  value       = mongodbatlas_network_container.container.id
   description = "The Network Peering Container ID"
 }
-
-# output "mongo_uri_updated" {
-#   value       = mongodbatlas_advanced_cluster.cluster.mongo_uri_updated
-#   description = "Lists when the connection string was last updated"
-# }
-
-# output "mongo_uri_with_options" {
-#   value       = mongodbatlas_advanced_cluster.cluster.mongo_uri_with_options
-#   description = "connection string for connecting to the Atlas cluster. Includes the replicaSet, ssl, and authSource query parameters in the connection string with values appropriate for the cluster"
-# }
-
-# output "connection_strings" {
-#   value       = mongodbatlas_advanced_cluster.cluster.connection_strings
-#   description = "Set of connection strings that your applications use to connect to this cluster"
-# }
-
-
-
-# output "paused" {
-#   value       = mongodbatlas_advanced_cluster.cluster.paused
-#   description = "Flag that indicates whether the cluster is paused or not"
-# }
-
-# output "srv_address" {
-#   value       = mongodbatlas_advanced_cluster.cluster.srv_address
-#   description = "Connection string for connecting to the Atlas cluster. The +srv modifier forces the connection to use TLS/SSL"
-# }
 
 output "state_name" {
   value       = mongodbatlas_advanced_cluster.cluster.state_name
